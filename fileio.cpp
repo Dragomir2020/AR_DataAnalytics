782b1c6a2719a8db54388dc3d490e4caca222005
wakcsur <wkacsur@umich.edu>
2017-02-04 06:35:42 -0500

    implement csv to txt conversion

--- fileio.cpp
+++ fileio.cpp
@@ -19,24 +19,26 @@
 using namespace std;
 
 
-void write_app_i_txt(ifstream& reader, ostream& ost, int i);
+void write_app_i_txt(ifstream& reader, ofstream& ost, int i);
 
 struct data 
 {
   string timestamp, num_cores, cpu_req, cpu_percent, cpu_mega_hertz, mem_provisioned, mem_usage, disk_read_through, disk_write_through;
   string network_rec_through, network_trans_through;
+  ofstream* ost;
 
   void print_data()
   {
-    cout << timestamp << " " << num_cores << " " << cpu_req  << " " << cpu_percent << " " << cpu_mega_hertz << " " << mem_provisioned;
-    cout << mem_usage << " " << disk_read_through << " " << disk_write_through  << " " << network_rec_through   << " ";
-    cout << network_trans_through << "\n";
+    *ost << timestamp << " " << num_cores << " " << cpu_req  << " " << cpu_percent << " " << cpu_mega_hertz << " " << mem_provisioned;
+    *ost << mem_usage << " " << disk_read_through << " " << disk_write_through  << " " << network_rec_through   << " ";
+    *ost << network_trans_through << "\n";
   }
 
 };
 
 struct device
 {
+  ofstream* ost;
   string device_name, environment, location, os, os_end_date;
   data* device_data;
 
@@ -44,19 +46,20 @@ struct device
 
   void print_device()
   {
-    cout << device_name << " " << environment << " " << location  << " " << os << " " << os_end_date << " " ;
+    *ost << device_name << " " << environment << " " << location  << " " << os << " " << os_end_date << " " ;
   }
 };
 
 struct app 
 {
   string app_name, business, priority;  
-  vector<device*> device_entries;;
+  vector<device*> device_entries;
+  ofstream * ost;
   int entry_count = 0;
 
   void print_app()
   {
-    cout << app_name << " " << business << " " << priority  << " ";
+    *ost << app_name << " " << business << " " << priority  << " ";
   }
 
 };
@@ -68,29 +71,35 @@ vector<app*> app_vec;
 
 int main(int argc, char ** argv)
 {
-  string prefix = "/csvfiles/app_";
+  string prefix = "app_";
   string csv = ".csv";
   string file_name = "";
   string num = "";
+  string write_to_file = "";
+  string read_from_file = "";
 
   int num_files = atoi(argv[1]); // 100
 
   ifstream reader;
   filebuf fb;
 
-  for (int i = 0; i < num_files; ++i)
+  for (int i = 1; i <= num_files; ++i)
     {
+
       num = to_string(i);
       file_name = prefix + num;
-      fb.open(/textfiles/file_name + ".txt" , ios::out);
-      ostream ost(&fb);
-      file_name += csv;
-      reader.open(file_name, ifstream::in);
-      
+      ofstream write_to;
+      write_to_file = "txtfiles/" + file_name + ".txt";
+      write_to.open(write_to_file);
+      read_from_file = "csvfiles/" + file_name + csv;
+   
+      reader.open(read_from_file, ifstream::in);
+
       //write to app_i.tx
-      write_app_i_txt(reader, ost, i);
+      write_app_i_txt(reader, write_to, i);
 
       reader.close();
+      write_to.close();
     }
 
 }
@@ -99,7 +108,7 @@ int main(int argc, char ** argv)
 
 
 //app: device_name priority timestamp data....
-void write_app_i_txt(ifstream& reader, ostream& ost, int i)
+void write_app_i_txt(ifstream& reader, ofstream& ost, int i)
 {
   
   
@@ -107,29 +116,33 @@ void write_app_i_txt(ifstream& reader, ostream& ost, int i)
   string device_name, environment, location, os, os_end_date;
   string timestamp, num_cores, cpu_req, cpu_percent, cpu_mega_hertz, mem_provisioned, mem_usage, disk_read_through, disk_write_through;
   string network_rec_through, network_trans_through;
+  string junk;
 
-  
-  
+  getline(reader, junk);
+
   while(getline(reader, device_name, ','))
     {
       app* cur_app = new app;
       app_vec.push_back(cur_app);
- 
       //read device inf
-      getline(reader, device_name, ',');
       getline(reader, environment, ',');
       getline(reader, location, ',');
       getline(reader, os, ',');
       getline(reader, os_end_date, ',');
       
-      cur_app->device_entries.push_back(new device(device_name, environment, location, os, os_end_date));     
-      
+      cur_app->device_entries.push_back(new device(device_name, environment, location, os, os_end_date)); 
+      cur_app->device_entries[cur_app->entry_count]->device_data = new data;
+      cur_app->device_entries[cur_app->entry_count]->ost = &ost;    
+      cur_app->ost = &ost;    
+
+
       //read app info
       getline(reader, cur_app->app_name, ',');
       getline(reader, cur_app->business, ',');
       getline(reader, cur_app->priority, ',');
  
       data* d_ptr = cur_app->device_entries[cur_app->entry_count]->device_data;
+      d_ptr->ost = &ost;
 
       //read data
       getline(reader, d_ptr->num_cores, ',');
@@ -150,5 +163,5 @@ void write_app_i_txt(ifstream& reader, ostream& ost, int i)
       ++cur_app->entry_count;
      
     }
-  
+  //cout << ost.str() << endl;
 }
